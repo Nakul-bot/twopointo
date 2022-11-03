ebeefb0df11c947cbec5dd0361267a750ce87007
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2022-11-04 03:54:15 +0530

    Implemented Slider
    Up and Running :muscle:

--- src/app/components/Slider.js
+++ src/app/components/Slider.js
@@ -68,10 +68,21 @@ export default class extends Component {
             clearTimeout(timeout)
             if (mouseOver) return
             timeout = setTimeout(() => {
-              slider.next()
+              // slider.next()
             }, 2000)
           }
           slider.on('created', () => {
+            window.nextSlide = () => {
+              if (slider.track.details.abs === 6) slider.moveToIdx(0)
+              else slider.next()
+
+              for (let slideIndex = 1; slideIndex <= 7; slideIndex += 1) {
+                const slide = slider.container.getElementsByClassName(`number-slide${slideIndex}`)[0]
+                const video = slide.children[0]
+                video.currentTime = 0
+              }
+            }
+
             slider.container.addEventListener('mouseover', () => {
               mouseOver = true
               clearNextTimeout()
@@ -84,7 +95,15 @@ export default class extends Component {
           })
           slider.on('dragStarted', clearNextTimeout)
           slider.on('animationEnded', nextTimeout)
-          slider.on('updated', nextTimeout)
+          slider.on('dragEnded', () => {
+            console.log('update')
+            const slideIndex = (slider.track.details.abs % slider.track.details.length) + 1
+            const slide = slider.container.getElementsByClassName(`number-slide${slideIndex}`)[0]
+            const video = slide.children[0]
+            video.currentTime = 0
+            video.play()
+            console.log(slide)
+          })
         }
       ]
     )
